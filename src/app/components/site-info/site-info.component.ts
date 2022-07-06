af29f046ab4d8f2544f73563ea44691161436d16
David Calderon <davidcalderon03@hotmail.com>
2022-07-06 09:23:05 -0500

    v8

--- src/app/components/site-info/site-info.component.ts
+++ src/app/components/site-info/site-info.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output, ElementRef, ViewChild }
   <a href={{webLink}} target="_blank">
   <img class="web-img" src={{pictureLink}} alt="" />
   </a>
-  <button class="detail-btn" (click)="customFunction()">
+  <button class="detail-btn" (click)="this.openContent(); this.createFocus(this.name)">
     {{highlighted? 'Go Back': 'Details'}}
   </button>
   <div #col class="collapsible-content" style="display: {{display}};">
@@ -124,11 +124,6 @@ export class SiteInfoComponent implements OnInit {
   public prevTop = 0;
   public prevScroll = 0;
 
-  public customFunction(){  
-    this.openContent(); 
-    this.createFocus(this.name);
-  }
-
   public openContent() {
     if(this.display === 'none'){
       this.display='block';
@@ -186,6 +181,9 @@ export class SiteInfoComponent implements OnInit {
       this.featuresRef.nativeElement.attributeStyleMap.clear();
       this.toolsRef.nativeElement.attributeStyleMap.clear();
       this.display = 'none';
+      if(window.innerWidth < 787) {
+        window.location.reload();
+      }
     }
   }
 
